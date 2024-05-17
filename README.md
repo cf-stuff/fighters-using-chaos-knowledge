74befeeaa8a302789e0a9bef516324ac89435734
cf-stuff <catform99@gmail.com>
2024-05-17 20:47:04 +1000

    rename stuff

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# CF Whale Simulator
-This aims to replicate battles in Chaos Fighters. Due to the nature of this game, it is impossible to 100% accurately depict certain behaviours (for example crit chance, dodge chance, bomb throwback chance, aotg switch chance). The best we can do is approximate them, so take the battle results with a grain a salt.
+# Fighters Using Chaos Knoweldge
 
-### https://cf-stuff.github.io/cf-whale-simulator/
+- Mod for https://cf-stuff.github.io/cf-whale-simulator/
+- Goal is to make more skills, fighters and pets viable
 
-## Not yet implemented
-- Different level fighter skills depending on fighter level
-- Various form validation issues
+### https://cf-stuff.github.io/fighters-using-chaos-knowledge/
--- js/components/App.js
+++ js/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return html`
   <header class="navbar navbar-expand-sm bg-light">
     <nav class="container-fluid">
-      <span class="navbar-brand">CF Whale Simulator</span>
+      <span class="navbar-brand">Fighters Using Chaos Knowledge</span>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -35,36 +35,8 @@ const App = () => {
     LINE ID: derpidgey<br/>
     Discord: CF99#9616
     <hr/>
-    <h3>Changelog</h3>
-    <p>2024-03-24: 1.8.5 - Updated Iron Man to reflect announced stats</p>
-    <p>2024-03-24: 1.8.4 - Added pet house</p>
-    <p>2024-03-20: 1.8.3 - Yellow totem now applies to both positive and negative status</p>
-    <p>2024-03-18: 1.8.2 - New builds in grading system to reflect current meta; fixed rebirth expertise effect not working</p>
-    <p>2024-03-12: 1.8.1 - Fixed strong wine fury burst cleansing itself</p>
-    <p>2024-03-07: 1.8.0 - Animated fights; fixed tired not being applied after explosive blow; time bomb stored damage now works properly</p>
-    <p>2024-02-16: 1.7.1 - Fixed bug causing rejuv not triggering in most situations</p>
-    <p>2023-04-17: 1.7.0 - Implemented shield wall burst and strong wine stat decreases; Rebirth chance down to 80% (90% with mastery)</p>
-    <p>2023-04-07: 1.6.1 - Rebirth and pet block no are longer affected by frenzy</p>
-    <p>2023-04-05: 1.6.0 - Increased crit probability; adjusted skill proc formula; fixed fire spam not triggering revive</p>
-    <p>2023-04-05: 1.5.6 - Updated SP% calculation to be consistent with in game; consume SP for ESW</p>
-    <p>2023-04-04: 1.5.5 - Fixed fury gained on every skill activation; skill planner improvements</p>
-    <p>2023-04-03: 1.5.4 - Fixed freezing/rebirth and needle/healing burst interactions</p>
-    <p>2023-03-31: 1.5.3 - Added skill planner</p>
-    <p>2023-03-30: 1.5.2 - Fixed esw damage</p>
-    <p>2023-03-28: 1.5.1 - Fixed ice to be more consistent with in game</p>
-    <p>2023-03-27: 1.5.0 - Fixed ordering of secondary skill effects and fury burst/rebirth</p>
-    <p>2023-03-26: 1.4.0 - Added grading; Implemented bomb throw back</p>
-    <p>2023-03-23: 1.3.2 - Display spd tiers and seconds per attack</p>
-    <p>2023-03-10: 1.3.1 - Added international pet icons</p>
-    <p>2023-02-27: 1.3.0 - Import and export builds via code; Sort saved builds in alphabetical order</p>
-    <p>2023-02-18: 1.2.5 - Prevent healing through energy shield</p>
-    <p>2023-02-17: 1.2.4 - Update star altar min and max atk from 40 to 50</p>
-    <p>2023-01-21: 1.2.3 - Added Hel</p>
-    <p>2022-11-16: 1.2.2 - Team battles</p>
-    <p>2022-11-14: 1.2.1 - Added all legendary fighters; Don't allow skills to be used while frozen</p>
-    <p>2022-11-13: 1.2.0 - Added battling</p>
-    <p>2022-11-12: 1.1.0 - Save and load builds locally</p>
-    <p>2022-11-11: 1.0.0 - Created calculator</p>
+    <h3>Mods</h3>
+    <p>Mole: new pet with special stat 150 EVA and special skill that gives golden shield</p>
   </div>
   `;
 }
